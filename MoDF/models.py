bd94d09f52ecf8ff222ef47317c3fe9adc7f8c8f
mlcb-jlu <74457986+mlcb-jlu@users.noreply.github.com>
2025-06-15 22:16:05 +0800

    Update models.py

--- MoDF/models.py
+++ MoDF/models.py
@@ -3,36 +3,6 @@ import torch
 import torch.nn.functional as F
 
 
-def xavier_init(m):
-    if type(m) == nn.Linear:
-        nn.init.xavier_normal_(m.weight)
-        if m.bias is not None:
-           m.bias.data.fill_(0.0)
-
-
-class Classifier_1(nn.Module):
-    def __init__(self, in_dim, out_dim):
-        super().__init__()
-        self.clf = nn.Sequential(nn.Linear(in_dim, out_dim))
-        self.clf.apply(xavier_init)
-
-    def forward(self, x):
-        x = self.clf(x)
-        return x
-
-
-class Classifier_2(nn.Module):
-    def __init__(self, in_dim, out_dim):
-
-        super().__init__()
-        self.clf = nn.Sequential(nn.Linear(in_dim, out_dim))
-        self.clf.apply(xavier_init)
-
-    def forward(self, x):
-        x = self.clf(x)
-        return x
-
-
 class Attention(nn.Module):
     def __init__(self, temperature, attn_dropout=0.1):
         super().__init__()
